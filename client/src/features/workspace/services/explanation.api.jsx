3c13fa8e9e722948b1513e265c95e694d47eda41
Dhanush Kumar <kanukantidhanushkumar944@gmail.com>
2026-08-02 20:25:56 +0530

    Replaced local apis with hosted apis

--- client/src/features/workspace/services/explanation.api.jsx
+++ client/src/features/workspace/services/explanation.api.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/explanation";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/explanation`;
 
 const getExplanation = async (props) => {
     const {
--- client/src/features/workspace/services/solution.api.jsx
+++ client/src/features/workspace/services/solution.api.jsx
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/solution";
+const API_URL = `${import.meta.env.VITE_API_URL}/api/solution`;
 
 const getSolution = async (props) => {
     const {
